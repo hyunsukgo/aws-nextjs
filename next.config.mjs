f31878279603f056b3f0ce0934c85cc0238a7a24
smilejakdu <ash982416@gmail.com>
2024-01-28 12:09:18 +0900

    next config js

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  distDir: 'build',
+  trailingSlash: true,
+  output: "export"
+};
 
 export default nextConfig;
